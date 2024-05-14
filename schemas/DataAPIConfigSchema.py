f1cae07b569d47d3f6ca6a21a8ae6da78eca82b7
Luke Swanson <lwswanson2@wisc.edu>
2024-05-14 16:00:02 -0500

    Add a schema for parsing out additional config data.

---
+++ schemas/DataAPIConfigSchema.py
@@ -0,0 +1,57 @@
+"""
+ServerConfigSchema
+
+Contains a Schema class for managing config data for server configurations.
+"""
+
+# import standard libraries
+import logging
+from pathlib import Path
+from typing import Any, Dict, List
+
+# import 3rd-party libraries
+
+# import OGD libraries
+from ogd.core.schemas.configs.data_sources.MySQLSourceSchema import MySQLSchema
+from ogd.apis.schemas.ServerConfigSchema import ServerConfigSchema
+
+# import local files
+
+class DataAPIConfigSchema(ServerConfigSchema):
+    def __init__(self, name:str, all_elements:Dict[str, Any], logger:logging.Logger):
+        self._state_dbs        : Dict[str, MySQLSchema]
+        self._ogd_core         : Path
+        self._google_client_id : str
+        self._dbg_level        : int
+        self._version          : int
+
+        if "GOOGLE_CLIENT_ID" in all_elements.keys():
+            self._google_client_id = ServerConfigSchema._parseGoogleID(google_id=all_elements["GOOGLE_CLIENT_ID"], logger=logger)
+        else:
+            self._google_client_id = "UNKNOWN ID"
+            logger.warn(f"{name} config does not have a 'GOOGLE_CLIENT_ID' element; defaulting to google_client_id={self._google_client_id}", logging.WARN)
+
+        _used = {"DB_CONFIG", "OGD_CORE_PATH", "GOOGLE_CLIENT_ID"}
+        _leftovers = { key : val for key,val in all_elements.items() if key not in _used }
+        super().__init__(name=name, all_elements=_leftovers, logger=logger)
+
+    @property
+    def GoogleClientID(self) -> str:
+        return self._google_client_id
+
+    @property
+    def AsMarkdown(self) -> str:
+        ret_val : str
+
+        ret_val = f"{self.Name}"
+        return ret_val
+
+    @staticmethod
+    def _parseGoogleID(google_id, logger:logging.Logger) -> str:
+        ret_val : str
+        if isinstance(google_id, str):
+            ret_val = google_id
+        else:
+            ret_val = str(google_id)
+            logger.warn(f"Google Client ID type was unexpected type {type(google_id)}, defaulting to google_client_id=str({ret_val}).", logging.WARN)
+        return ret_val
